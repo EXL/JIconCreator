5dd1f4ca0a324ceacc3c7db0ee3c01351c31a252
EXL <exlmotodev@gmail.com>
2018-08-25 04:48:00 +0700

    GUI Form: realize showing generated icons

--- src/main/java/ru/exlmoto/jiconcreator/JIconCreatorForm.java
+++ src/main/java/ru/exlmoto/jiconcreator/JIconCreatorForm.java
@@ -1,6 +1,12 @@
 package ru.exlmoto.jiconcreator;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 public class JIconCreatorForm {
     private JPanel mainPanel;
@@ -16,16 +22,87 @@ public class JIconCreatorForm {
     private JSlider paddingSlider;
     private JButton squareButton;
     private JButton circleButton;
-    private JPanel mdpiPane;
-    private JPanel hdpiPane;
-    private JPanel xhdpiPane;
-    private JPanel xxhdpiPane;
+    private JLabel mdpiLabel;
+    private JLabel hdpiLabel;
+    private JLabel xhdpiLabel;
+    private JLabel xxhdpiLabel;
+
+    private CreateAssetSetWizardState createAssetSetWizardState = null;
 
     private void createUIComponents() {
+        createAssetSetWizardState = new CreateAssetSetWizardState();
+
         // TODO: place custom component creation code here
+/*
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File("test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (image != null)
+            mdpiLabel.setIcon(new ImageIcon(image));
+*/
+        /*
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File("test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (image != null) {
+            final BufferedImage finalBufferedImage = image;
+            mdpiPane = new JPanel() {
+
+                @Override
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(finalBufferedImage , 0, 0, null);
+                }
+            };
+
+            mdpiPane.setSize(48, 48);
+        } */
+    }
+
+    // public???
+    public void updatePreviewIcons() {
+        int cnt = 0;
+        Map<String, Map<String, BufferedImage>> categories = JIconCreator.generateImages(createAssetSetWizardState, false);
+        for (Map<String, BufferedImage> previews : categories.values()) {
+            for (Map.Entry<String, BufferedImage> entry : previews.entrySet()) {
+                BufferedImage image = entry.getValue();
+
+                if (image != null) {
+                    switch (cnt) {
+                        default:
+                            return;
+                        case 0:
+                            mdpiLabel.setIcon(new ImageIcon(image));
+                            break;
+                        case 1:
+                            hdpiLabel.setIcon(new ImageIcon(image));
+                            break;
+                        case 2:
+                            xhdpiLabel.setIcon(new ImageIcon(image));
+                            break;
+                        case 3:
+                            xxhdpiLabel.setIcon(new ImageIcon(image));
+                            break;
+                    }
+                    cnt++;
+                } else {
+                    return;
+                }
+            }
+        }
     }
 
     public JPanel getMainPane() {
+        createUIComponents();
+        updatePreviewIcons();
         return mainPanel;
     }
 }
