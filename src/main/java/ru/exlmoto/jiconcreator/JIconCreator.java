e12fb5ddca424baef663589748a6644decd635eb
EXL <exlmotodev@gmail.com>
2019-04-20 09:50:12 +0700

    Migrate to BoxLayout from FreeLayout

--- src/main/java/ru/exlmoto/jiconcreator/JIconCreator.java
+++ src/main/java/ru/exlmoto/jiconcreator/JIconCreator.java
@@ -28,17 +28,7 @@ public class JIconCreator extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
