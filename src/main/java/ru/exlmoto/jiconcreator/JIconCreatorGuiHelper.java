ae185a5cc9434888927b98139aa9edfdd8ecd3bb
EXL <exlmotodev@gmail.com>
2019-04-21 14:05:51 +0700

    Rewrite fill Menu Style method

--- src/main/java/ru/exlmoto/jiconcreator/JIconCreatorGuiHelper.java
+++ src/main/java/ru/exlmoto/jiconcreator/JIconCreatorGuiHelper.java
@@ -4,52 +4,25 @@ import ru.exlmoto.jiconcreator.unsorted.CreateAssetSetWizardState;
 import ru.exlmoto.jiconcreator.unsorted.JIconCreatorExtrasLibraryHere;
 import ru.exlmoto.jiconcreator.unsorted.RGB;
 
-import javax.swing.JRadioButtonMenuItem;
-import javax.swing.UIManager;
+import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.SwingUtilities;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.awt.Color;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class JIconCreatorGuiHelper {
-    private HashMap<String, String> installedStyles = null;
-
+class JIconCreatorGuiHelper {
     private CreateAssetSetWizardState createAssetSetWizardState = null;
     private JIconCreatorGui jIconCreatorGui = null;
 
-    public JIconCreatorGuiHelper(JIconCreatorGui jIconCreatorGuiArg) {
+    JIconCreatorGuiHelper(JIconCreatorGui jIconCreatorGuiArg) {
         jIconCreatorGui = jIconCreatorGuiArg;
 
         createAssetSetWizardState = new CreateAssetSetWizardState();
-    }
-
-    /************/
-    public void imageTabBackColorRandomButton() {
-        int red = new Random().nextInt(255 + 1);
-        int green = new Random().nextInt(255 + 1);
-        int blue = new Random().nextInt(255 + 1);
-        System.out.println("Clicked Random button: " + red + " " + green + " " + blue);
-        createAssetSetWizardState.background = new RGB(red, green, blue);
-        jIconCreatorGui.getLabelColorShowImageL().setBackground(new Color(red, green, blue));
-        updatePreviewIcons();
-    }
-
-    public void updatePreviewIcons(/*labels*/) {
-        int cnt = 0;
-        Map<String, Map<String, BufferedImage>> categories = JIconCreatorExtrasLibraryHere.generateImages(createAssetSetWizardState, true);
-        if (wtf(cnt, categories,
-                jIconCreatorGui.getLabelMdpiI(), jIconCreatorGui.getLabelHdpiI(), jIconCreatorGui.getLabelXhdpiI(), jIconCreatorGui.getLabelXxhdpiI())) return;
-
-        System.gc();
     }
 
     static boolean wtf(int cnt, Map<String, Map<String, BufferedImage>> categories, JLabel labelMdpiImage, JLabel labelHpdiImage, JLabel labelXhdpiImage, JLabel labelXxhdpiImage) {
@@ -82,27 +55,56 @@ public class JIconCreatorGuiHelper {
         }
         return false;
     }
+
+    /************/
+    void imageTabBackColorRandomButton() {
+        int red = new Random().nextInt(255 + 1);
+        int green = new Random().nextInt(255 + 1);
+        int blue = new Random().nextInt(255 + 1);
+        System.out.println("Clicked Random button: " + red + " " + green + " " + blue);
+        createAssetSetWizardState.background = new RGB(red, green, blue);
+        jIconCreatorGui.getLabelColorShowImageL().setBackground(new Color(red, green, blue));
+        updatePreviewIcons();
+    }
+
+    void updatePreviewIcons(/*labels*/) {
+        int cnt = 0;
+        Map<String, Map<String, BufferedImage>> categories = JIconCreatorExtrasLibraryHere.generateImages(createAssetSetWizardState, true);
+        if (wtf(cnt, categories,
+                jIconCreatorGui.getLabelMdpiI(), jIconCreatorGui.getLabelHdpiI(), jIconCreatorGui.getLabelXhdpiI(), jIconCreatorGui.getLabelXxhdpiI()))
+            return;
+
+        System.gc();
+    }
+
     /************/
 
-    public void generateStyleMenuItems(/* buttonGroupStyles */ /* jMenuStyle */ /* this */) {
+    void generateStyleMenuItems() {
+        HashMap<String, String> installedStyles = new HashMap<>();
+        ArrayList<JRadioButtonMenuItem> styleMenuItems = new ArrayList<>();
+
+        LookAndFeelInfo[] styles = UIManager.getInstalledLookAndFeels();
+
         boolean isWindows = false;
-        installedStyles = new HashMap<String, String>();
-        ArrayList<JRadioButtonMenuItem> styleMenuItems = new ArrayList<JRadioButtonMenuItem>();
-        for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+
+        for (LookAndFeelInfo info : styles) {
             String styleName = info.getName();
+
             styleMenuItems.add(new JRadioButtonMenuItem(styleName));
             installedStyles.put(styleName, info.getClassName());
+
             if (styleName.startsWith("Windows")) {
                 isWindows = true;
             }
         }
+
         for (final JRadioButtonMenuItem menuItem : styleMenuItems) {
-            if (UIManager.getLookAndFeel().getName().equals(menuItem.getText()) && !isWindows) {
-                menuItem.setSelected(true);
-            }
             if (menuItem.getText().equals("Windows") && isWindows) {
                 menuItem.setSelected(true);
+            } else if (UIManager.getLookAndFeel().getName().equals(menuItem.getText())) {
+                menuItem.setSelected(true);
             }
+
             menuItem.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     try {
@@ -113,9 +115,11 @@ public class JIconCreatorGuiHelper {
                     }
                 }
             });
+
             jIconCreatorGui.getButtonGroupStyles().add(menuItem);
             jIconCreatorGui.getMenuStyle().add(menuItem);
         }
+
         if (isWindows) {
             try {
                 UIManager.setLookAndFeel(installedStyles.get("Windows"));
